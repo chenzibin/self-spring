05f691070cd73a0a3af910b9c32980ab11549493
zibin chen <chenzb@broada.com>
2019-11-25 18:05:29 +0800

    spring framework

---
+++ self-spring-framework/src/main/java/self/spring/framework/web/servlet/DispatcherServlet.java
@@ -0,0 +1,12 @@
+package self.spring.framework.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * DispatcherServlet
+ *
+ * @author chenzb
+ * @date 2019/11/25
+ */
+public class DispatcherServlet extends HttpServlet {
+}
---
+++ self-spring-framework/src/test/java/self/spring/framework/BaseTest.java
@@ -0,0 +1,15 @@
+package self.spring.framework;
+
+/**
+ * BaseTest
+ *
+ * @author chenzb
+ * @date 2019/11/25
+ */
+public class BaseTest {
+
+    public static void main(String[] args) {
+        System.out.println("dad");
+    }
+
+}
