04c216be5c8ac3c598dc07d4b1fb5fa6fe908ed0
zibin chen <chenzb@broada.com>
2020-02-27 17:12:59 +0800

    ioc

---
+++ self-spring-geek/src/main/java/self/spring/geek/ioc/domain/SuperUser.java
@@ -0,0 +1,10 @@
+package self.spring.geek.ioc.domain;
+
+/**
+ * SuperUser
+ *
+ * @author chenzb
+ * @date 2020/2/27
+ */
+public class SuperUser {
+}
---
+++ self-spring-geek/src/main/java/self/spring/geek/ioc/domain/User.java
@@ -0,0 +1,10 @@
+package self.spring.geek.ioc.domain;
+
+/**
+ * User
+ *
+ * @author chenzb
+ * @date 2020/2/27
+ */
+public class User {
+}
