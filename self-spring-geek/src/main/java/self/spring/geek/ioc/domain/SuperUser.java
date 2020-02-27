b2117f1a1e07a74f80fb2c9badd4b50fe1c17a97
zibin chen <chenzb@broada.com>
2020-02-27 17:16:38 +0800

    ioc

--- self-spring-geek/src/main/java/self/spring/geek/ioc/domain/SuperUser.java
+++ self-spring-geek/src/main/java/self/spring/geek/ioc/domain/SuperUser.java
@@ -1,10 +1,16 @@
 package self.spring.geek.ioc.domain;
 
+import lombok.Data;
+
 /**
  * SuperUser
  *
  * @author chenzb
  * @date 2020/2/27
  */
-public class SuperUser {
+@Data
+public class SuperUser extends User {
+
+    private String address;
+
 }
--- self-spring-geek/src/main/java/self/spring/geek/ioc/domain/User.java
+++ self-spring-geek/src/main/java/self/spring/geek/ioc/domain/User.java
@@ -1,10 +1,17 @@
 package self.spring.geek.ioc.domain;
 
+import lombok.Data;
+
 /**
  * User
  *
  * @author chenzb
  * @date 2020/2/27
  */
+@Data
 public class User {
+
+    private String name;
+
+    private int age;
 }
